a7bca6ae3aafe16d175961b987cc56d68216d463
OdeliaCohen <odeliacohen1503@gmail.com>
2024-01-27 18:40:02 +0100

    creation de bdd & migration

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -6,9 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class InscriptionController extends AbstractController
-{
-    #[Route('/inscription', name: 'app_inscription')]
+class InscriptionController extends AbstractController{
+    #[Route('/inscription', name: 'budget_inscription')]
     public function index(): Response
     {
         return $this->render('inscription/index.html.twig', [
