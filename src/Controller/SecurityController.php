27930c97a9384cfd7a940808b70d1e454c0863de
SaraSerhal <saraserhal07@gmail.com>
2024-04-30 05:44:51 +0200

    FEAT update password (css needed to be fixed)

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,11 +2,15 @@
 
 namespace App\Controller;
 
+use App\Form\UpdatePasswordType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 
 class SecurityController extends AbstractController
 {
@@ -29,6 +33,29 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route(path: '/update-password', name: 'update_password')]
+    public function updatePassword(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+
+        $form = $this->createForm(UpdatePasswordType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword($userPasswordHasher->hashPassword($user, $form->get('plainPassword')->getData()));
+
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Your password has been updated.');
+
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('security/updatePassword.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 
 
